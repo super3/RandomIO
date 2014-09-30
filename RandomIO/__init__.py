6ae377d24bc5102a8fa90a7240a577ff8e53e35c
Will James <jameswt@gmail.com>
2014-09-30 20:04:09 +1000

    modified license

--- RandomIO/__init__.py
+++ RandomIO/__init__.py
@@ -1,7 +1,7 @@
 #
 # The MIT License (MIT)
 #
-# Copyright (c) 2014 William T. James
+# Copyright (c) 2014 William T. James for Storj Labs
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
