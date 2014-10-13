d26f7fd3f91a554de7c790a2a8063d827ea0b8cd
Josh Brandoff <josh@joshua.is>
2014-10-13 13:46:21 -0400

    PEP8 Styling

--- tests/tests-unit.py
+++ tests/tests-unit.py
@@ -26,7 +26,9 @@ import os
 
 import RandomIO
 
+
 class TestRandomIO(unittest.TestCase):
+
     def setUp(self):
         pass
 
@@ -36,22 +38,25 @@ class TestRandomIO(unittest.TestCase):
     def test_gen(self):
         s = RandomIO.RandomIO()
         b = s.read(100)
-        
-        self.assertEqual(len(b),100)
-        
-        self.assertEqual(RandomIO.RandomIO(123456).read(100),RandomIO.RandomIO(123456).read(100))
-        self.assertEqual(RandomIO.RandomIO(b'byte string seed').read(100),RandomIO.RandomIO(b'byte string seed').read(100))
-        self.assertEqual(RandomIO.RandomIO(1.23456).read(100),RandomIO.RandomIO(1.23456).read(100))
-        
+
+        self.assertEqual(len(b), 100)
+
+        self.assertEqual(
+            RandomIO.RandomIO(123456).read(100), RandomIO.RandomIO(123456).read(100))
+        self.assertEqual(RandomIO.RandomIO(b'byte string seed').read(
+            100), RandomIO.RandomIO(b'byte string seed').read(100))
+        self.assertEqual(RandomIO.RandomIO(1.23456).read(
+            100), RandomIO.RandomIO(1.23456).read(100))
+
     def test_consistent(self):
         s1 = RandomIO.RandomIO('seed string')
         s2 = RandomIO.RandomIO('seed string')
         s3 = RandomIO.RandomIO('seed string')
         s4 = RandomIO.RandomIO('another seed')
-        
-        self.assertEqual(s1.read(100),s2.read(100))
-        self.assertNotEqual(s3.read(100),s4.read(100))
-        
+
+        self.assertEqual(s1.read(100), s2.read(100))
+        self.assertNotEqual(s3.read(100), s4.read(100))
+
     def test_crossplatform(self):
         string_seed1 = b'\t\xb0\xef\xd9\x05p\xe1W\x17\x8a9\xc6!;^6\x1d\xadj\
 \xb4#n\x1d/\x12+\xe6\xb1\x80\xc86\x06I\xc4!\x8b39\x84E\x1d\x14\xdf\x14e\x12\
@@ -59,90 +64,89 @@ class TestRandomIO(unittest.TestCase):
 
         s = RandomIO.RandomIO('seed1').read(50)
 
-        self.assertEqual(s,string_seed1)
-        
+        self.assertEqual(s, string_seed1)
+
         string_123456 = b'\x18\xb2\xce\x8a \xc9\xe2n\xd9\xf6\x06\x0b8\xf9\xb9\
 \xf8\x9b#81z\xf8\x02\x83\x1e\xa2\xf02\x7f\xad\xd7*h\xad9\xf6\x14U\xca\x90\\i\
 \xcc~#h\xaa\xb4\x1b['
 
         s = RandomIO.RandomIO(123456).read(50)
-        
-        self.assertEqual(s,string_123456)
-        
+
+        self.assertEqual(s, string_123456)
+
     def test_read(self):
         s1 = RandomIO.RandomIO('seed string')
-        
+
         with self.assertRaises(RuntimeError) as ex:
             s1.read()
-            
-        self.assertEqual(str(ex.exception),'Size must be greater than zero')
-        
+
+        self.assertEqual(str(ex.exception), 'Size must be greater than zero')
+
     def test_dump(self):
         s1 = RandomIO.RandomIO('seed string')
         s2 = RandomIO.RandomIO('seed string')
-    
+
         file1 = 'file1'
         file2 = 'file2'
-        
-        with open(file1,'wb') as f:
-            s1.dump(100,f)
-        
-        with open(file2,'wb') as f:
-            s2.dump(100,f)
-        
-        with open(file1,'rb') as f:
+
+        with open(file1, 'wb') as f:
+            s1.dump(100, f)
+
+        with open(file2, 'wb') as f:
+            s2.dump(100, f)
+
+        with open(file1, 'rb') as f:
             contents1 = f.read()
-        
-        with open(file2,'rb') as f:
+
+        with open(file2, 'rb') as f:
             contents2 = f.read()
-            
-        self.assertEqual(len(contents1),100)
-        self.assertEqual(contents1,contents2)
-        
+
+        self.assertEqual(len(contents1), 100)
+        self.assertEqual(contents1, contents2)
+
         os.remove(file1)
         os.remove(file2)
 
     def test_genfile(self):
         path = RandomIO.RandomIO('seed string').genfile(100)
-        
-        with open(path,'rb') as f:
+
+        with open(path, 'rb') as f:
             contents = f.read()
-        
-        self.assertEqual(len(contents),100)
-        
+
+        self.assertEqual(len(contents), 100)
+
         os.remove(path)
-        
+
         dir = 'test_directory/'
-        
+
         os.mkdir(dir)
-        path = RandomIO.RandomIO('seed string').genfile(100,dir)
-        
-        (h1,t1) = os.path.split(dir)
-        (h2,t2) = os.path.split(path)
-        self.assertEqual(h1,h2)
-        
-        with open(path,'rb') as f:
+        path = RandomIO.RandomIO('seed string').genfile(100, dir)
+
+        (h1, t1) = os.path.split(dir)
+        (h2, t2) = os.path.split(path)
+        self.assertEqual(h1, h2)
+
+        with open(path, 'rb') as f:
             contents = f.read()
-            
-        self.assertEqual(len(contents),100)
-        
+
+        self.assertEqual(len(contents), 100)
+
         os.remove(path)
         os.rmdir(dir)
-        
-        
+
     def test_large(self):
         length = 100000000
         file1 = RandomIO.RandomIO('seed string').genfile(length)
         file2 = RandomIO.RandomIO('seed string').genfile(length)
-        
-        with open(file1,'rb') as f1:
-            with open(file1,'rb') as f2:
-                for c in iter(lambda:f1.read(1000),b''):
-                    self.assertEqual(c,f2.read(1000))
-                    
+
+        with open(file1, 'rb') as f1:
+            with open(file1, 'rb') as f2:
+                for c in iter(lambda: f1.read(1000), b''):
+                    self.assertEqual(c, f2.read(1000))
+
         os.remove(file1)
         os.remove(file2)
-        
-        
+
+
 if __name__ == '__main__':
     unittest.main()
